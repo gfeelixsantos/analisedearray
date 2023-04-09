d70c3418f1ac8aee259af93219798ffe0b0dbc09
gfeelixsantos <felix.devx@gmail.com>
2023-04-09 15:54:01 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,80 @@
+//INPUTS: Número e botão
+let entradaNumero = document.querySelector('#numero')
+entradaNumero.addEventListener('keyup', inputCheck)
+
+let enviar = document.querySelector('#enviar')
+
+let resultados = document.querySelector('#resultados')
+resultados.setAttribute("Size","2")
+enviar.addEventListener('click', addarray)
+
+let gerar = document.querySelector('#gerar')
+gerar.addEventListener('click', gerarResultados)
+
+let arraylist = document.getElementById('arraylist')
+
+let botaoLimpar = document.querySelector('#botaoLimpar')
+botaoLimpar.addEventListener('click', limpar)
+
+let lista = [] 
+
+
+function inputCheck (entradaNumero){
+
+    const char = String.fromCharCode(entradaNumero.keyCode)
+    console.log(char);
+    
+}
+const patternInput = '[0-9][.]{3}'
+
+
+
+function addarray(){ 
+    //CRIA ARRAY E ADICIONA ELEMENTOS ENVIADOS
+    resultados.appendChild(document.createElement('option')).innerHTML=`${entradaNumero.value}`
+    return lista.push(entradaNumero.value)  
+}
+
+function gerarResultados(){
+    //ARPESENTA NUMEROS ENVIADOS
+    document.querySelector('#lista').innerHTML= `Os números enviados foram: ${lista}`
+
+    //ORDENANDO O ARRAY
+    let novoArray = lista.sort(ordenar)
+    
+    //MENOR NÚMERO
+    document.querySelector('#menorNumero').innerHTML=`O menor número enviado foi: ${novoArray[0]}`
+
+    //MAIOR NÚMERO
+    novoArray.reverse()
+    document.querySelector('#maiorNumero').innerHTML=`O maior número enviado foi: ${novoArray[0]}`
+
+    //LISTA ORDENADA
+    novoArray.reverse()
+    document.querySelector('#ordenado').innerHTML= `Lista ordenada: ${novoArray}` 
+    
+    //SOMATÓRIA
+    let resultadoSoma = 0
+    for (let index = 0; index < novoArray.length; index++) {
+        
+        //CONVERTENDO ARRAY DE STRINGS PARA NUMBER
+        numberArray = novoArray.map(Number)
+        resultadoSoma += numberArray[index] 
+    }
+    document.querySelector('#somatoria').innerHTML=`A soma dos números são: ${resultadoSoma}`
+
+    //MEDIA PONDERADA
+    document.querySelector('#media').innerHTML= `Sua média ponderada é: ${(resultadoSoma / novoArray.length).toFixed(2)}`
+}
+
+
+//FUNÇÃO PARA ATUALIZAR A PÁGINA
+function limpar() {
+    window.location.reload(true)
+}
+
+//FUNÇÃO PARA ORDENAR ARRAY
+function ordenar (a, b){
+    return a - b
+}
+
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Projeto: Analise de números array #
+----
+_Este projeto tem o objetivo de analisar os números enviados pelo usuário, armazenados em um array, retornando as informações como: lista ordenada dos números enviados, menor número encontrado, maior número encontrado e média ponderada._ 
+
+<div align="center">
+
+![](./css/assets/preview.png)
+
+</div>
+
+----
+Esta solução foi proposta através do [Curso em vídeo](https://www.cursoemvideo.com/curso/javascript/) na trilha de JavaScript.
+
+## Tecnologias Desenvolvidas ##
+- HTML
+- CSS
+- JavaScript
+
+<br>
+
+### Minhas redes ###
+[![](./css/assets/icon-likedin.png)](https://www.linkedin.com/in/gabriel-felix-dos-santos-85ba259b/) [![](./css/assets/icon-instagram.png)](https://www.instagram.com/feliquitox/) [![](./css/assets/icon-facebook.png)](https://www.facebook.com/gabriel.felix.50/)
